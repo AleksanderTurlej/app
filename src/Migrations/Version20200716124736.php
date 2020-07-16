9f6379cc42d0120be4b957bae782e7fc48f21e58
Aleksander <https://github.com/AleksanderTurlej/app>
2020-07-16 16:06:28 +0000

    migrations_issue

--- src/Migrations/Version20200716124736.php
+++ src/Migrations/Version20200716124736.php
@@ -20,7 +20,7 @@ final class Version20200716124736 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE medicine CHANGE uploud_file upload_file VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE medicine ADD COLUMN upload_file VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
